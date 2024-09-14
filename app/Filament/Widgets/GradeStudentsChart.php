dd6bb332d0dfebda3296b4d014cf41881b11b7b3
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2024-09-14 10:16:04 +0530

    chart added

---
+++ app/Filament/Widgets/GradeStudentsChart.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\ChartWidget;
+use App\Models\Student;
+use App\Models\Grade;
+use Illuminate\Support\Facades\DB;
+
+class GradeStudentsChart extends ChartWidget
+{
+    protected static ?string $heading = 'GradeStudents Overview';
+
+    protected function getData(): array
+    {
+        $labels = Grade::take(5)->pluck('grade_name')->toArray();
+
+        $data = Student::join('grades', 'students.grade_id', '=', 'grades.id')
+                       ->select('grades.grade_name', DB::raw('count(students.id) as total'))
+                       ->groupBy('grades.grade_name')
+                       ->get();
+        $studentCounts = $data->pluck('total')->toArray();
+       
+            return [
+                'datasets' => [
+                    [
+                        'label' => 'Students',
+                        'data' => $studentCounts,
+                    ],
+                ],
+                'labels' => $labels,
+            ];
+        
+    }
+
+    protected function getType(): string
+    {
+        return 'bar';
+    }
+}
